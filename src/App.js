6e5409bdc452bfdaedf5217b3abea62ce67b1adf
Sadra <sadra.kabiri@gmail.com>
2024-02-06 10:43:22 +0330

    push

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Homepage from "./components/Homepage";
-import ProductsFunc from "./components/Products/ProductsFunc"
-import ProductDetail from './components/Products/Productsco/ProductDetail';
-import { Route, Routes , Navigate,  } from "react-router-dom";
-import ProductsContexProvider from './components/Products/ProductsContexProvider';
-import CardContextProvider from './components/Cart/CartContextProvider';
-import Cart from './components/Cart/Cart';
+import ProductsFunc from "./components/Products/ProductsFunc";
+import ProductDetail from "./components/Products/Productsco/ProductDetail";
+import { Route, Routes, Navigate } from "react-router-dom";
+import ProductsContexProvider from "./components/Products/ProductsContexProvider";
+import CardContextProvider from "./components/Cart/CartContextProvider";
+import Cart from "./components/Cart/Cart";
 class App extends Component {
-    render() {
-        return (
-            <>
-
-
-                <ProductsContexProvider>
-                    <CardContextProvider>
-                        <Navbar />
-                        <Routes>
-
-                            <Route path="/products/:id" element={<ProductDetail/>} />
-                            <Route path="/cart" element={<Cart/>} />
-                            <Route path="/products" element={<ProductsFunc/>} />
-                            <Route exact path="/" element={<Homepage/>} />
-                            <Route path='/*' element={<Navigate to="/" />}/>
-                        </Routes>
-                    </CardContextProvider>
-                </ProductsContexProvider>
-                <Footer />
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <ProductsContexProvider>
+          <CardContextProvider>
+            <Navbar />
+            <Routes>
+              <Route path="/products/:id" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/products" element={<ProductsFunc />} />
+              <Route exact path="/" element={<Homepage />} />
+              <Route path="/*" element={<Navigate to="/" />} />
+            </Routes>
+          </CardContextProvider>
+        </ProductsContexProvider>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
